e62d4c43a42689c56593c7dcafa2adfe5c8fc46d
Francesco Biegi <francesco.biegi@ympact.xyz>
2026-03-22 20:34:21 +0100

    feat: added test endpoint

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('ping', function () {
-    return response()->json(['message' => 'pong service-a release-please-test-fix (test) -6']);
+    return response()->json(['message' => 'pong service-a test']);
 });
 
 Route::get('pong', function () {
