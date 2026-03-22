77164b0b6bc79211911f8f2c1ba2ad390488eee7
Francesco Biegi <francesco.biegi@gmail.com>
2026-03-22 23:00:37 +0100

    feat: test workflow (#42)
    Co-authored-by: Francesco Biegi <francesco.biegi@ympact.xyz>

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('ping', function () {
-    return response()->json(['message' => 'PR test workflow']);
+    return response()->json(['message' => 'PR test workflow (1)']);
 });
 
 Route::get('pong', function () {
