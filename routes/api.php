b6d76beccb8e0d601db0b6211d43b5e564b0ced6
Francesco Biegi <francesco.biegi@gmail.com>
2026-03-22 15:44:03 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('ping', function () {
-    return response()->json(['message' => 'pong service-a release-please-test-fix (test) -2']);
+    return response()->json(['message' => 'pong service-a release-please-test-fix (test) -3']);
 });
