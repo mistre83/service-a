461908a34f5af9ee14fa79f8c2881eeef640f5c5
Francesco Biegi <francesco.biegi@ympact.xyz>
2026-03-22 22:15:38 +0100

    feat: test size label

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('ping', function () {
-    return response()->json(['message' => 'PR on test']);
+    return response()->json(['message' => 'PR on test -size label']);
 });
 
 Route::get('pong', function () {
